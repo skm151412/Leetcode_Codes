f868c4ae63d7f543d6b82081472260a43e6b052d
SOURAV KUMAR <skm151412@gmail.com>
2026-08-14 10:45:20 +0530

    Time: 0 ms (100%), Space: 47.9 MB (94.89%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int search(int[] a, int t) {
+        int l=0,r=a.length-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(a[m]==t) return m;
+            else if(a[m]>t) r=m-1;
+            else l=m+1;
+        }
+        return -1;
+    }
+}
