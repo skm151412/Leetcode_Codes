75526254e31f910b4e2d8090421261086f554d72
SOURAV KUMAR <skm151412@gmail.com>
2026-09-02 11:26:59 +0530

    Time: 0 ms (100%), Space: 45.5 MB (5.31%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int o=0,e=0;
-        for(int x:nums1){
-            if(x%2==0) o++;
-            else e++;
-        }
-        if(e==0 || o==0) return true;
-        if(o>=1) return true;
-        return false;
+        // int o=0,e=0;
+        // for(int x:nums1){
+        //     if(x%2==0) o++;
+        //     else e++;
+        // }
+        // if(e==0 || o==0) return true;
+        // if(o>=1) return true;
+        // return false;
+        return true;
     }
 }
